a20b73c4896a71b1bddf5bd0399f270a92946201
dwvwdv <dwvwdv@protonmail.com>
2022-11-24 16:24:30 +0800

    init.

---
+++ BBSWebAPI/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BBSWebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            string connString = "server=localhost;database=bbsdb;";
+            SqlConnection conn = new SqlConnection(connString);
+            //conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from users");
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            sda.Fill(ds);
+
+            DataTable res = ds.Tables[0];
+            DataRow dr = res.Rows[0];
+            var value = dr["userNo"].ToString();
+
+            return value;
+        }
+        [HttpPost]
+        public string Insert(string user,string password)
+        {
+            return "dwv Post";
+        }
+        [HttpPut]
+        public string Update()
+        {
+            return "dwv Put";
+        }
+        [HttpDelete]
+        public string Remove()
+        {
+            return "dwv Del";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# 論壇開發練手作品
+
+## 使用語言與技術  
+### Frontend  
+- Undecided...
+### Backend  
+- C#
+- MS SQL
+- Docker?
